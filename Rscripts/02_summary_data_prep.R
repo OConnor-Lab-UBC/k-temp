3c8f25f914646f7ed82799209fb675a480fa528e
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-02-22 12:33:54 -0800

    adding feb 22 flow cam data and renaming feb 19 files to include date

--- Rscripts/02_summary_data_prep.R
+++ Rscripts/02_summary_data_prep.R
@@ -8,11 +8,11 @@ chlamy_summary <- read_csv("/Users/Joey/Documents/chlamy-ktemp/k-temp/data-summa
 str(chlamy_summary)
 
 
-fnams <- list.files("data-summary", full.names = TRUE) ## find out the names of all the files in data-raw, use full.names to get the relative path for each file
+fnams <- list.files("data-summary", full.names = TRUE) ## find out the names of all the files in data-summary, use full.names to get the relative path for each file
 fnams
 
 chlamy_summaries <- fnams %>%  
-		lapply(FUN = function(p) read.csv(p)) %>%
+		lapply(FUN = function(p) read.csv(p)) %>% 
 		as.data.frame(.) %>% 
 	filter(List.File == "Particles / ml") %>% 
 	select(- starts_with("List")) %>%
@@ -23,11 +23,9 @@ chlamy_summaries <- fnams %>%
 	select(-V1)
 
 chlamy_summaries %>% 
-	ggplot(., aes(x = dataset, y = cell_count)) + geom_bar(stat= "identity")
+	ggplot(., aes(x = dataset, y = cell_count)) + geom_bar(stat= "identity") + theme(axis.text.x = element_text(size = 20, angle = 90, hjust = 1))
 	
 
-# chlamy_summaries %>% 
-# 	separate(., dataset, c("name", "replicate", "temperature"), extra = "drop",  sep = ".") %>% View
 
 
-
+# separate(chlamy_summaries, dataset, c("name", "replicate", "temperature", "date", "type"), extra = "drop",  sep = ".") %>% View
